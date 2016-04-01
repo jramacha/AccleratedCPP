2776fbe871aa9d52b26b6a573bbe18f8d74bcef0
Jayanth Ramachandran <jramachandran@jayanths-macbook-air.local>
2016-04-01 06:21:54 -0400

    Initial Commit

---
+++ AccleratedCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AccleratedCPP
+//
+//  Created by Jayanth Ramachandran on 4/1/16.
+//  Copyright © 2016 Jayanth Ramachandran. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
